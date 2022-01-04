7977e897eb08206bf327bbe41d7e4ea28c339d39
saklain404 <mohdsaklain8899@gmail.com>
2022-01-05 00:46:57 +0530

    gh pages added

---
+++ src/Components/Icons.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {FaPen, FaTimes, FaRegCircle} from 'react-icons/fa'
+
+const Icons =({name}) => {
+        switch (name) {
+            case "circle": 
+
+              return <FaRegCircle className="icon"/>
+
+            case "cross":
+
+            return <FaTimes className="icon"/>
+
+            default : 
+               return <FaPen className="icon"/>
+        }
+}
+
+
+export default Icons
